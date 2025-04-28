a4b4c8ef311d1f0ec646809fdecc41d03f9cf7d6
alemalagni <alemalagni@gmail.com>
2025-04-28 17:06:00 +0200

    feat(PostsPage): add PostsList

--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -1,7 +1,10 @@
+import PostsList from './PostsList';
+
 function PostsPage({ posts }) {
     return (
         <div>
             <h1>Lista dei Post</h1>
+            <PostsList posts={posts} />
         </div>
     );
 }
