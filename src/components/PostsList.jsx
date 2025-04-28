be6aae9a3c29228b4d72e7873852168d7f81bf04
alemalagni <alemalagni@gmail.com>
2025-04-28 23:08:17 +0200

    style(PostsList): added

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,7 +6,7 @@ function PostsList({ posts }) {
     const { posts } = useContext(PostsContext);
 
     return (
-        <div>
+        <div style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
             {posts.map(post => (
                 <PostCard key={post.id} title={post.title} content={post.content} />
             ))}
