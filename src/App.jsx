84676b61b9b55168439f48668327cbc531e1e20d
alemalagni <alemalagni@gmail.com>
2025-04-28 16:58:55 +0200

    feat(app.jsx): add posts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import PostsPage from './PostsPage';
 import './App.css'
 
 function App() {
+  const [posts] = useState([
+    { id: 1, title: 'Primo Post', content: 'Primo post.' },
+    { id: 2, title: 'Secondo Post', content: 'Secondo post.' },
+    { id: 3, title: 'Terzo Post', content: 'Terzo post.' }
+  ]);
+
   return (
     <>
 
