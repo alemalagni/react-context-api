11f0597bdb43db8c0db232f7ce383859ef52cf28
alemalagni <alemalagni@gmail.com>
2025-04-28 17:18:18 +0200

    feat(PostsList): add return

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,5 +1,13 @@
-function PostsList({ posts }) {
+import PostCard from './PostCard';
 
+function PostsList({ posts }) {
+    return (
+        <div>
+            {posts.map(post => (
+                <PostCard key={post.id} title={post.title} content={post.content} />
+            ))}
+        </div>
+    );
 }
 
 export default PostsList;
